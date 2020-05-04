11c3c0994f7a5f26371c4bd9d1da04f7d3fdf53c
Michael <Boltmichaelt@gmail.com>
2020-05-04 10:41:15 -0400

    Created buttons.h/c to provide button debouncing/reading.

---
+++ buttons.h
@@ -0,0 +1,25 @@
+/*
+ * buttons.h
+ *
+ *  Created on: May 4, 2020
+ *      Author: Michael Bolt
+ */
+
+#ifndef BUTTONS_H_
+#define BUTTONS_H_
+
+
+#include <msp430.h>
+#include <stdint.h>
+
+// button definitions
+#define BTN_UP      BIT2
+#define BTN_DOWN    BIT1
+#define BTN_SHOOT   BIT0
+
+// functions
+void buttons_init(void);
+inline uint16_t readButton(uint16_t button);
+
+
+#endif /* BUTTONS_H_ */
--- main.c
+++ main.c
@@ -7,13 +7,10 @@
 
 #include <msp430.h>
 #include <stdint.h>
+#include "buttons.h"
 #include "i2c.h"
 #include "ssd1306.h"
-#include <math.h>
-#include <stdio.h>
 
-// global variable to indicate if button is pressed
-volatile uint8_t btnState = 0;
 
 // main
 void main(void) {
@@ -22,38 +19,30 @@ void main(void) {
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
 
-    // i2c init
+    buttons_init();
     i2c_init();
     ssd1306_init();
     // configure P1.0 as output for LED
     P1DIR |=  BIT0 | BIT1;
     P1OUT &= ~(BIT0 | BIT1);
-    // configure P2.[0:2] for a pushbutton input
-    P2DIR &=  ~(BIT0 | BIT1 | BIT2); // input
-    P2REN |=   (BIT0 | BIT1 | BIT2); // enable pull-up/down resistor
-    P2OUT |=   (BIT0 | BIT1 | BIT2); // set to pull-up
-    // enable 5 ms timer interrupt with timerA module
-    TA0CTL   |= TACLR;        // clear timer logic
-    TA0CTL   |= TASSEL__ACLK; // source = ACLK (32 kHz)
-    TA0CCR0   = 164;          // period = 164 / 2^15 = 5 ms
-    TA0CCTL0 |= CCIE;         // enable CCR0 interrupt
-    TA0CTL   |= MC__UP;       // mode = UP
 
     // enable interrupts
     __bis_SR_register(GIE);
 
+    // sprite to draw for shoot button
     const uint8_t arrow[8] = {0x18,0x18,0x18,0x18,0x99,0x5A,0x3C,0x18};
     uint16_t x = 0;
 
+    // eternal loop
     while(1) {
         // UP button = red LED
-        if(btnState & BIT2) P1OUT |=  BIT0;
-        else                P1OUT &= ~BIT0;
+        if(readButton(BTN_UP))      P1OUT |=  BIT0;
+        else                        P1OUT &= ~BIT0;
         // DOWN button = green LED
-        if(btnState & BIT1) P1OUT |=  BIT1;
-        else                P1OUT &= ~BIT1;
+        if(readButton(BTN_DOWN))    P1OUT |=  BIT1;
+        else                        P1OUT &= ~BIT1;
         // SHOOT button = sprite!
-        if(btnState & BIT0) {
+        if(readButton(BTN_SHOOT)) {
             display_frameStart();
             display_drawSprite(x, 12, arrow);
             display_drawFrame();
@@ -64,27 +53,5 @@ void main(void) {
     }
 }
 
-//******************************************************************************
-// Timer A0 CCR0 ISR ***********************************************************
-//******************************************************************************
-#pragma vector = TIMER0_A0_VECTOR
-__interrupt void Timer_A (void)
-{
-    // static state buffer and index retain values between interrupts
-    static uint8_t state[8] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-    static uint16_t index = 0;
-    // add pushbutton samples to state buffer
-    state[index++] = P2IN;
-    // wrap index value to length of state buffer (modulo 8)
-    index &= 0x07;
-    // OR entire state buffer to check for 1's
-    uint16_t i = 0;
-    uint8_t temp = 0;
-    for(i = 0; i < 8; temp |= state[i++]);
-    // store stable button presses as 1's
-    btnState = 0xFF ^ temp;
-    // breakpoint here to check operation
-    __no_operation();
-}
 
 
