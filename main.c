4e41273d5f1df403b868460bb4bc052c787d4813
Michael <Boltmichaelt@gmail.com>
2020-05-04 09:00:27 -0400

    Port-wide 40ms debounce implemented.

--- main.c
+++ main.c
@@ -6,52 +6,85 @@
  */
 
 #include <msp430.h>
+#include <stdint.h>
+#include "i2c.h"
+#include "ssd1306.h"
+#include <math.h>
+#include <stdio.h>
 
-volatile unsigned int btnCount = 0;
+// global variable to indicate if button is pressed
+volatile uint8_t btnState = 0;
 
+// main
 void main(void) {
 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
 
+    // i2c init
+    i2c_init();
+    ssd1306_init();
     // configure P1.0 as output for LED
-    P1DIR |=  BIT0;
-    P1OUT &= ~BIT0;
-    // configure P2.0 for a pushbutton interrupt
-    P2DIR &=  BIT0; // input
-    P2REN |=  BIT0; // enable pull-up/down resistor
-    P2OUT |=  BIT0; // set to pull-up
-    P2IES |=  BIT0; // interrupt on high-low transition
-    P2IFG &= ~BIT0; // make sure interrupt flag is cleared
-    P2IE  |=  BIT0; // enable interrupt
+    P1DIR |=  BIT0 | BIT1;
+    P1OUT &= ~(BIT0 | BIT1);
+    // configure P2.[0:2] for a pushbutton input
+    P2DIR &=  ~(BIT0 | BIT1 | BIT2); // input
+    P2REN |=   (BIT0 | BIT1 | BIT2); // enable pull-up/down resistor
+    P2OUT |=   (BIT0 | BIT1 | BIT2); // set to pull-up
+    // enable 5 ms timer interrupt with timerA module
+    TA0CTL   |= TACLR;        // clear timer logic
+    TA0CTL   |= TASSEL__ACLK; // source = ACLK (32 kHz)
+    TA0CCR0   = 164;          // period = 164 / 2^15 = 5 ms
+    TA0CCTL0 |= CCIE;         // enable CCR0 interrupt
+    TA0CTL   |= MC__UP;       // mode = UP
 
-    // enter low power mode to wait for interrupts
-    __bis_SR_register(GIE + LPM0_bits);
+    // enable interrupts
+    __bis_SR_register(GIE);
+
+    const uint8_t arrow[8] = {0x18,0x18,0x18,0x18,0x99,0x5A,0x3C,0x18};
+    uint16_t x = 0;
+
+    while(1) {
+        // UP button = red LED
+        if(btnState & BIT2) P1OUT |=  BIT0;
+        else                P1OUT &= ~BIT0;
+        // DOWN button = green LED
+        if(btnState & BIT1) P1OUT |=  BIT1;
+        else                P1OUT &= ~BIT1;
+        // SHOOT button = sprite!
+        if(btnState & BIT0) {
+            display_frameStart();
+            display_drawSprite(x, 12, arrow);
+            display_drawFrame();
+            x++;
+            x &= 127;
+        }
+        __delay_cycles(10000);
+    }
 }
 
 //******************************************************************************
-// Button Interrupts *************************************************************
+// Timer A0 CCR0 ISR ***********************************************************
 //******************************************************************************
-#pragma vector = PORT2_VECTOR
-__interrupt void P2_ISR(void)
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
 {
-  switch(__even_in_range(P2IV, 0x1F))
-  {
-    case P2IV_NONE:    break;        // Vector 0: No interrupts
-    case P2IV_P2IFG0:                // Vector 2: P2.0
-        P1OUT ^= BIT0;      // toggle LED
-        btnCount++;         // increment counter
-        __no_operation();   // set breakpoint here to check btnCount
-        break;
-    case P2IV_P2IFG1:  break;        // Vector 4: P2.1
-    case P2IV_P2IFG2:  break;        // Vector 6: P2.2
-    case P2IV_P2IFG3:  break;        // Vector 8: P2.3
-    case P2IV_P2IFG4:  break;        // Vector 10: P2.4
-    case P2IV_P2IFG5:  break;        // Vector 12: P2.5
-    case P2IV_P2IFG6:  break;        // Vector 14: P2.6
-    case P2IV_P2IFG7:  break;        // Vector 16: P2.7
-    default:
-        break;
-  }
+    // static state buffer and index retain values between interrupts
+    static uint8_t state[8] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+    static uint16_t index = 0;
+    // add pushbutton samples to state buffer
+    state[index++] = P2IN;
+    // wrap index value to length of state buffer (modulo 8)
+    index &= 0x07;
+    // OR entire state buffer to check for 1's
+    uint16_t i = 0;
+    uint8_t temp = 0;
+    for(i = 0; i < 8; temp |= state[i++]);
+    // store stable button presses as 1's
+    btnState = 0xFF ^ temp;
+    // breakpoint here to check operation
+    __no_operation();
 }
+
+
